2ba2b3952eff1520a47daa63e0cbeba8c9f60183
batrakrishna20 <batrakrishna20@gmail.com>
2026-01-21 17:25:31 +0530

    Implement prime number check in Q1.C

--- LAB_1/Q1.C
+++ LAB_1/Q1.C
@@ -1,1 +1,28 @@
+#include <stdio.h>
+
+int main() {
+    int n, i, isPrime = 1;
+
+    printf("Enter a number: ");
+    scanf("%d", &n);
+
+    // Numbers less than 2 are not prime
+    if (n < 2) {
+        isPrime = 0;
+    } else {
+        for (i = 2; i * i <= n; i++) {
+            if (n % i == 0) {
+                isPrime = 0;
+                break;
+            }
+        }
+    }
+
+    if (isPrime)
+        printf("%d is a prime number.\n", n);
+    else
+        printf("%d is not a prime number.\n", n);
+
+    return 0;
+}
 
